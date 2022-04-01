1b0999279ef0535b23dbafb9d91ed66991e6a8b2
Nancy Hernandez <hernandezyanezhotmail.com>
2022-04-01 02:43:52 -0300

    creating context and theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
-import { useState } from "react";
+import { Container, Grid, Typography } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { NewsProvider } from "./context/NewsProvider";
+import Form from "./components/Form";
+
+const theme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#006778",
+    },
+    secondary: {
+      main: "#FFD124",
+    },
+  },
+});
 
 function App() {
   return (
-    <div>
-      <h1>News app</h1>
-    </div>
+    <NewsProvider>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="sm">
+          <header>
+            <Typography
+              align="center"
+              my={5}
+              component="h1"
+              variant="h2"
+              color="primary"
+            >
+              News<span> 🗞</span> Browser
+            </Typography>
+          </header>
+
+          <Grid
+            container
+            // direction="row"
+            justifyContent="center"
+            mx={-2}
+          >
+            <Grid item xs={12} md={6} sx={{ margin: "0 auto" }}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </ThemeProvider>
+    </NewsProvider>
   );
 }
 
